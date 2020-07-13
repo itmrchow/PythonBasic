8d5892e8c9018b602cb2c5203aa81f0b849b827e
itmrchow <itmrchow@gmail.com>
2020-07-13 13:06:52 +0800

    if else 練習

--- loopAndIfElse.py
+++ loopAndIfElse.py
@@ -7,3 +7,12 @@ while b < 10:
     list.append(b)
 
 print(b, sep=",")
+
+# if else
+index = 0
+while index < 10:
+    if index % 2 == 0:
+        print(index, ":偶數")
+    else:
+        print(index, ":奇數")
+    index += 1
