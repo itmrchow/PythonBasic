5772197cd8262afcf4e097d3ae3fdb57dd089458
itmrchow <itmrchow@gmail.com>
2020-07-16 11:15:59 +0800

    Iteration class練習

---
+++ MyInterationClass.py
@@ -0,0 +1,18 @@
+class MyClass:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+
+myClass = MyClass()
+myIter = iter(myClass)
+
+print("MyIteration")
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
