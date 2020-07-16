c78eaad2fa93a74db2b47b25b7b8642becac9419
itmrchow <itmrchow@gmail.com>
2020-07-16 11:14:43 +0800

    while loop next練習

--- IteratorAndGenerator.py
+++ IteratorAndGenerator.py
@@ -1,9 +1,25 @@
-#iter and next
+# iter and next
+import sys
+
 mList = [1, 2, 3, 4]
 it = iter(mList)
+print("iter and next")
 print(next(it))
 print(next(it))
 
 # for迴圈遍歷iter
+print("for遍歷iter")
 for i in it:
     print(i, end=" ")
+
+print("\n")
+
+# while loop next
+mList2 = [1, 2, 3, 4]
+it2 = iter(mList2)
+print("while遍歷iter")
+while True:
+    try:
+        print(next(it2))
+    except StopIteration:
+        sys.exit()
