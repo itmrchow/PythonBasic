f47512016968b23c40d5e8c7c3823fff992e489f
itmrchow <itmrchow@gmail.com>
2020-07-16 11:26:58 +0800

    Iteration class註解

--- MyInterationClass.py
+++ MyInterationClass.py
@@ -1,8 +1,10 @@
 class MyClass:
+    # 返回迭代器對象，會實作next方法並透過stopiteration來標示完成
     def __iter__(self):
         self.a = 1
         return self
 
+    # 返回下一個迭代器對象
     def __next__(self):
         x = self.a
         self.a += 1
