f36f7eb9aa7cedf244e82900e54eed959ff80788
itmrchow <itmrchow@gmail.com>
2020-07-28 13:31:26 +0800

    def傳不可變、可變對象練習

--- def.py
+++ def.py
@@ -1,6 +1,40 @@
-# first def
 def helloWorld():
+    """
+    : first def
+    """
     print("Hello World")
 
 
+def area(width, height):
+    """
+    : 帶參數def
+    """
+    return width*height
+
+
+def changeInt(input_i):
+    """
+    :傳不可變對象
+    """
+    input_i = 10
+    return input_i
+
+
+def changeMe(inputList):
+    """
+    :傳可變對象
+    """
+    for i in range(len(inputList)):
+        inputList[i] = inputList[i]*2
+
+
 helloWorld()
+print(area(5, 3))
+
+b = 2
+changeInt(b)
+print(b)
+
+cList = ['a', 'b', 'c']
+changeMe(cList)
+print(cList)
