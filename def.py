0068bc277431bd985fea4e1262b58613e5725394
itmrchow <itmrchow@gmail.com>
2020-07-28 14:10:04 +0800

    def參數設定練習

--- def.py
+++ def.py
@@ -28,6 +28,22 @@ def changeMe(inputList):
         inputList[i] = inputList[i]*2
 
 
+def printMe(str1, str2):  # 必須參數
+    print("第一組:"+str1)
+    print("第二組:"+str2)
+
+
+def printInfo(arg1, *vartuple):  # 不定長參數 *tuple
+    print(arg1, end=",")
+    for var in vartuple:
+        print(var, end=",")
+
+
+def printInfo_2(arg1, **vardict):  # 不定長參數 *tuple
+    print(arg1)
+    print(vardict)
+
+
 helloWorld()
 print(area(5, 3))
 
@@ -38,3 +54,9 @@ print(b)
 cList = ['a', 'b', 'c']
 changeMe(cList)
 print(cList)
+
+printMe("hello", "world")
+printMe(str2="JJJJJ", str1="YYYY")
+
+printInfo(4, 5, 6, 7, 8)
+printInfo_2(4, a=2, b=3)
