05362a1c7bf16692b335a3688753bd5fb30400da
Jeff <jeff.wu@shoalter.com>
2022-04-11 17:01:04 +0800

    feat: user insert & delete

---
+++ flaskCRUD/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restful import Api
+
+from resources.user import User
+from resources.user import Users
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(User, "/user/<string:name>")
+api.add_resource(Users, "/users")
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ flaskCRUD/resources/user.py
@@ -0,0 +1,45 @@
+from flask_restful import Resource
+
+users = [{
+    'name': 'Jeff',
+}]
+
+
+class User(Resource):
+    def get(self, name):
+        find = [item for item in users if item['name'] == name]
+        if len(find) == 0:
+            return {
+                'message': 'username not exist!'
+            }, 403
+
+        user = find[0]
+        if not user:
+            return {
+                'message': 'username not exist!'
+            }, 403
+        return {
+            'message': '',
+            'user': user
+        }
+
+    def post(self, name):
+        pass
+
+    def put(self, name):
+        pass
+
+    def delete(self, name):
+        global users
+        users = [item for item in users if item['name'] != name]
+        return {
+            'message': 'Delete done'
+        }
+
+
+class Users(Resource):
+    def get(self):
+        return {
+            'message': '',
+            'users': users
+        }
