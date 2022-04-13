4f0467f53cca68e61149c7464f5a87b63256bff5
Jeff <jeff.wu@shoalter.com>
2022-04-13 16:05:51 +0800

    feat:add mysql setting & Migrate

--- flaskCRUD/app.py
+++ flaskCRUD/app.py
@@ -1,7 +1,9 @@
 from common.ma import ma
 from common.db import db
+
 from flask import Flask
 from flask_restful import Api
+from flask_migrate import Migrate
 
 
 from resources.user import User
@@ -9,16 +11,19 @@ from resources.user import Users
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@127.0.0.1:3306/pytest'
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "mysql+pymysql://root:123456@127.0.0.1:3306/pytest"
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS "] = False
 api = Api(app)
 
 api.add_resource(User, "/user/<string:name>")
 api.add_resource(Users, "/users")
 
-print("__name__:" + __name__)
-
 
 db.init_app(app)
+migrate = Migrate(app, db)
 ma.init_app(app)
 
 app.run(debug=True)
